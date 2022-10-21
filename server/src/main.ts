d13af52c0c196eeb34f515edad085276f023a75e
Peter571 <peterkoech1624@gmail.com>
2022-10-21 08:42:17 +0300

    origin change

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ "origin": 'https://dai-escrow.netlify.app/'});
+  app.enableCors({ "origin": '*'});
   await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();
+//https://dai-escrow.netlify.app/
